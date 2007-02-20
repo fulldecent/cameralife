9ee83cfd19f9142edc07abf56e8ae996a26087a5
fulldecent <fulldecent@cb962ef4-2621-0410-b4d7-891680544ae6>
2007-02-20 03:44:58 +0000

    semantics

--- modules/core/stats.class.php
+++ modules/core/stats.class.php
@@ -52,33 +52,33 @@ class Stats
     if (empty($this->counts))
       $this->GetCounts();
 
-    $funfacts[] = 'If these photos were taken with a film camera, they would have used '.(round($this->counts['photos'] / 24, 0)).' rolls of film.';
+    $funfacts[] = 'If these photos were taken with a film camera, they would have used <strong>'.(round($this->counts['photos'] / 24, 0)).'</strong> rolls of film.';
     $funfacts[] = 'If the photos were layed on a football field, they would go up to the '.
-                  '<b>'.(round($this->counts['pixels'] / 358318080,2)).'</b> yard line.';
+                  '<strong>'.(round($this->counts['pixels'] / 358318080,2)).'</strong> yard line.';
                   # 358318080 = 160ft * 1 yd * 3ft/yd * 144 in^2/ft^2 * 5184 px^2/in^2
     $funfacts[] = 'If the photo pixels were layed 1-wide, they would circle '.
-                  '<b>'.(round($this->counts['pixels'] / 1135990288,2)).'%</b> of the world.';
+                  '<strong>'.(round($this->counts['pixels'] / 1135990288,2)).'%</strong> of the world.';
                   # 1135990288 = 3963.21mi * 2pi * 1760 yd/mi * 36 in/yd * 72 px/in / 100%
     $funfacts[] = 'If I had a nickel for every time someone looked at a picture here, I would have '.
-                  '<b>$'.(floor($this->counts['photohits'] / 20)).'</b>.';
+                  '<strong>$'.(floor($this->counts['photohits'] / 20)).'</strong>.';
     $funfacts[] = 'There have been an average of '.
-                  '<b>'.(round ($this->counts['photos'] / ($this->counts['daysonline'] + 1),3)).'</b> photos posted every day.';
+                  '<strong>'.(round ($this->counts['photos'] / ($this->counts['daysonline'] + 1),3)).'</strong> photos posted every day.';
     $funfacts[] = 'If you printed these photos and stacked them, they would be '.
-                  '<b>'.(round ($this->counts['photos'] / 60,2)).'</b> inches high.';
+                  '<strong>'.(round ($this->counts['photos'] / 60,2)).'</strong> inches high.';
     $funfacts[] = 'It would take '.
-                  '<b>'.(round ($this->counts['photos'] / 350,0)).' shoeboxes</b> to store all these photos.';
+                  '<strong>'.(round ($this->counts['photos'] / 350,0)).' shoeboxes</strong> to store all these photos.';
     $funfacts[] = 'Printing these photos on an inkjet printer would use '.
-                  '<b>'.(round ($this->counts['photos'] / 11,0)).'</b> cartridges costing '.
-                  '<b>$'.(round ($this->counts['photos'] / 11 * 13,0)).'</b> retail.';
+                  '<strong>'.(round ($this->counts['photos'] / 11,0)).'</strong> cartridges costing '.
+                  '<strong>$'.(round ($this->counts['photos'] / 11 * 13,0)).'</strong> retail.';
                   # http://www.epinions.com/content_141398871684
     $funfacts[] = 'Printing these photos with the leading online print service would cost '.
-                  '<b>$'.(round ($this->counts['photos'] * 0.15, 0)).'</b>.';
+                  '<strong>$'.(round ($this->counts['photos'] * 0.15, 0)).'</strong>.';
                   # http://www.shutterfly.com/help/pop/pricing.jsp#volume
     $funfacts[] = 'Putting all these photos on your refrigerator will require '.
-                  '<b>'.(round ($this->counts['photos'] / 64, 0)).' refrigerators</b>.';
+                  '<strong>'.(round ($this->counts['photos'] / 64, 0)).' refrigerators</strong>.';
                   # Model General Electric GTS18FBSWW
     $funfacts[] = 'Postage for mailing a photo here to each of your friends (like you have that many) will cost '.
-                  '<b>$'.(round ($this->counts['photos'] * 0.39, 2)).'</b>.';
+                  '<strong>$'.(round ($this->counts['photos'] * 0.39, 2)).'</strong>.';
                   # Model General Electric GTS18FBSWW
     return $funfacts;
   }
