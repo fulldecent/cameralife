63a6a71c673ed4d1eceef12f6414b8441891eefd
fulldecent <fulldecent@cb962ef4-2621-0410-b4d7-891680544ae6>
2009-04-16 00:56:57 +0000

    Add the requested widgetwq

---
+++ hacks/widget.php
@@ -0,0 +1,23 @@
+<?php
+  $features=array('database','security','imageprocessing', 'photostore');
+  require "../main.inc";
+  $cameralife->base_url = dirname($cameralife->base_url);
+
+  echo '<html><head><title>Pics</title></head><body>';
+  echo '<h1 style="font-size:medium; border-left: 13px solid #003377; padding-left: 5px ">Photos from '.$cameralife->GetPref('sitename').'</h1>';
+
+  $photos = new Search();
+  $photos->SetPage(0, 3);
+  $photos->SetSort('rand');
+  foreach ($photos->GetPhotos() as $photo)
+  {
+    $icon = $photo->GetIcon();
+    echo "          <a href=\"".$icon['href']."\">\n";
+    echo "            <img style=\"border:0\" src=\"".$icon['image']."\" width=\"".$icon['width']."\" height=\"".$icon['height
+']."\" ".
+                      " alt=\"".htmlentities($icon['name'])."\" />\n";
+    echo "          </a>\n";
+  }
+  echo '</body></html>';
+?>
+
