398ef6a3a475b2ae53e3da3aedf072e2b52cda6d
William Entriken <github@phor.net>
2012-10-20 14:49:39 -0400

    update to latest lightopenid

--- modules/security/openid/process.php
+++ modules/security/openid/process.php
@@ -1,27 +1,28 @@
 <?php
-require 'openid.php';
 
-  $features=array('database','theme','security');
-  require "../../../main.inc";
-  $cameralife->base_url = dirname(dirname(dirname($cameralife->base_url)));
+$features=array('database','theme','security');
+require "../../../main.inc";
+$cameralife->base_url = dirname(dirname(dirname($cameralife->base_url)));
 
+require 'lightopenid/openid.php';
 
 try {
     # Mewp told me specifically not to use SERVER_NAME.
     # Change 'localhost' to your domain name.
 
     $openid = new LightOpenID($_SERVER['SERVER_NAME']);
-    $openid->required = array('contact/email');
-
     if(!$openid->mode) {
         if(isset($_POST['openid_identifier'])) {
             $openid->identity = $_POST['openid_identifier'];
+            # The following two lines request email, full name, and a nickname
+            # from the provider. Remove them if you don't need that data.
+            $openid->required = array('contact/email');
+            $openid->optional = array('namePerson', 'namePerson/friendly');
             header('Location: ' . $openid->authUrl());
         }
     } elseif($openid->mode == 'cancel') {
-        die ('User has canceled authentication!');
+        echo 'User has canceled authentication!';
     } else {
-        $valid = false;
         $id = "";
         $email = "";
         if ($openid->validate())
@@ -29,16 +30,15 @@ try {
             $id = $openid->identity;
             $attr = $openid->getAttributes();
             $email = $attr['contact/email'];
-            if(strlen($email))
-                $valid = true;
-        }
-        if (!$openid->validate())
+            if (strlen($email)) {
+                $cameralife->Security->Login($id, $email);
+                header ('Location: '.$cameralife->base_url.'/index.php');
+            } else {
+                die ('Enough detail (email address) was not provided to process your login.');
+            }
+        } else {
             die ('Provider did not validate your login');
-        if (!$valid)
-            die ('Enough detail (email address) was not provided to process your login.');
-
-        $cameralife->Security->Login($id, $email);
-        header ('Location: '.$cameralife->base_url.'/index.php');
+        }
     }
 } catch(ErrorException $e) {
     echo $e->getMessage();
