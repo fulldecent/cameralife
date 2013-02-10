2d952620f90ef20fdea3c25e4035fe3c2fa3dd33
William Entriken <github@phor.net>
2013-02-10 13:47:56 -0500

    Use bootstrap for admin/files

--- admin/files.php
+++ admin/files.php
@@ -1,26 +1,21 @@
 <?php
-/**
- * Handle file things
+/*
+ * Administer files on the site
  * BEWARE: BACKUP YOUR DATABASE BEFORE MESSING AROUND HERE!
- * if only I had told myself that earlier today :-(
- *
  * @author Will Entriken <cameralife@phor.net>
- * @copyright Copyright (c) 2001-2009 Will Entriken
+ * @copyright Copyright (c) 2001-2013 Will Entriken
  * @access public
  */
-@ini_set('max_execution_time',9000);
-
-$features=array('database','security','imageprocessing', 'photostore');
+$features=array('database','security', 'photostore');
 require "../main.inc";
-$cameralife->base_url = dirname($cameralife->base_url);
 chdir ($cameralife->base_dir);
-$cameralife->Security->authorize('admin_file', 1); // Require
-
+$cameralife->base_url = dirname($cameralife->base_url);
+$cameralife->Security->authorize('admin_customize', 1); // Require
 $_GET['page'] or $_GET['page'] = 'flagged';
+@ini_set('max_execution_time',9000); // for rescan
 
 // Handle form actions
-foreach ($_POST as $key=>$val)
-{
+foreach ($_POST as $key=>$val) {
   if (!is_int($key)) continue;
   $curphoto = new Photo($key);
   if ($val>=0 && $val<=3)
@@ -28,119 +23,127 @@ foreach ($_POST as $key=>$val)
   else // Erased file
     $curphoto->Erase();
 }
-
 ?>
-<html>
-<head>
-  <title><?= $cameralife->GetPref('sitename') ?></title>
-  <link rel="stylesheet" href="admin.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-  <script language="javascript">
-    function changeall() {
-      val = document.getElementById('status').value;
-      inputs = document.getElementsByTagName('select');
-      for (var i = 0; i < inputs.length; i++) {
-          inputs[i].value=val;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Camera Life - Administration</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
       }
-    }
-  </script>
-</head>
-<body>
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+    <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 
-<div id="header">
-<h1>Site Administration &ndash; File Manager</h1>
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <span class="brand"><a href="../"><?= $cameralife->GetPref("sitename") ?></a> / <a href="index.php">Administration</a> / File Manager</span>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <ul class="nav nav-tabs">
+        <li<?= ($_GET['page']=='flagged')?' class="active"':'' ?>><a href="?page=flagged">Flagged files</a></li>
+        <li<?= ($_GET['page']=='private')?' class="active"':'' ?>><a href="?page=private">Private</a></li>
+        <li<?= ($_GET['page']=='update')?' class="active"':'' ?>><a href="?page=update">Rescan files</a></li>
+      </ul>
+  <?php
+
+if ($_GET['page'] == 'flagged')
+  $target_status = 1;
+else if ($_GET['page'] == 'private')
+  $target_status = 2;
+else if ($_GET['page'] == 'upload')
+  $target_status = 3;
+
+if ($_GET['page'] !== 'update') // Show stuff
+{
+?>
+      <div class="pull-right well">
+        <h1>Quick tools</h1>
+        <p>Set all status to...</p>
+        <select name="status" onchange="$('select').val($('#status').val())" id="status">';
+          <option value="0">Public</option>
+          <option value="1">Flagged</option>
+          <option value="2">Private</option> 
+          <option value="3">New Upload</option>
+          <option value="4">Erased</option>
+        </select>
+      </div>
+      <form method="post">
 <?php
-  $home = $cameralife->GetIcon('small');
-  echo '<a href="'.$home['href']."\"><img src=\"".$cameralife->IconURL('small-main')."\">".$home['name']."</a>\n";
-?> |
-<a href="index.php"><img src="<?= $cameralife->IconURL('small-admin')?>">Site Administration</a>
-</div>
-
-<form method="post" action="http://<?= $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'] ?>&#63;page=<?= $_GET['page']?>">
-<h2>
-  Show:
-  <a href="?page=flagged">Flagged photos</a> |
-  <a href="?page=private">Private photos</a> |
-  <a href="?page=update">Rescan photos</a>
-</h2>
-<?php
-
   if ($_GET['page'] == 'flagged')
-    $target_status = 1;
+    echo "<p>Photos that are flagged show up here. \"Erasing\" a photo deletes it, unless <a href=\"photostore.php\">your photostore</a> is set up to keep a copy."; 
   else if ($_GET['page'] == 'private')
-    $target_status = 2;
+    echo '<p>Photos that have been marked private will show here.</p>';
   else if ($_GET['page'] == 'upload')
-    $target_status = 3;
+    echo '<p>Photos that have been uploaded by users will show here.</p>';
 
-  if ($_GET['page'] !== 'update') // Show stuff
-  {
-    if ($_GET['page'] == 'flagged')
-      echo "<p class='administrative'>Photos that have been flagged will show up here. If you 'erase' a photo, if will be deteled from your photostore. Some photostores will keep a copy of deleted photos. You can configure photostores <a href='photostore.php'>here</a>. You may send flagged photos to the private photo section.</p>";
-    else if ($_GET['page'] == 'private')
-      echo '<p class="administrative">Photos that have been marked private will show here.</p>';
-    else if ($_GET['page'] == 'upload')
-      echo '<p class="administrative">Photos that have been uploaded by users will show here.</p>';
+  $search = new Search('');
+  $search->mySearchPhotoCondition = "status=$target_status OR 0";
+  $search->SetPage(0, 9999);
+  $photos = $search->GetPhotos();
+  $icons = array();
 
-    echo '<p>Change All: <select name="status" onchange="changeall()" id="status">';
-    echo '<option value="0" '.($target_status==0?'selected':'').'>Public</option>';
-    echo '<option value="1" '.($target_status==1?'selected':'').'>Flagged</option>';
-    echo '<option value="2" '.($target_status==2?'selected':'').'>Private</option>';
-    echo '<option value="3" '.($target_status==3?'selected':'').'>New Upload</option>';
-    echo '<option value="4" '.($target_status==4?'selected':'').'>Erased</option>';
-    echo '</select></p>';
-
-    $search = new Search('');
-    ### @todo You know code is a hack when you use a SQL injection attack against yourself.
-    $search->mySearchPhotoCondition = "status=$target_status OR 0";
-    $search->SetPage(0, 9999);
-    $photos = $search->GetPhotos();
-    $icons = array();
-
-    echo '<table>';
-
-    foreach($photos as $photo)
-    {
-      if (!($i++%4)) echo '<tr>';
-
-      $icon = $photo->GetIcon();
-      echo '<td align="center" width="25%">';
-      echo '<a href="'.$icon['href'].'">';
-      echo '<img src="'.$icon['image'].'"></a><br />';
-      echo '<select name="'.$photo->Get('id').'">'.
-                        '<option value="0" '.($target_status==0?'selected':'').'>Public</option>'.
-                        '<option value="1" '.($target_status==1?'selected':'').'>Flagged</option>'.
-                        '<option value="2" '.($target_status==2?'selected':'').'>Private</option>'.
-                        '<option value="3" '.($target_status==3?'selected':'').'>New Upload</option>'.
-                        '<option value="4" '.($target_status==4?'selected':'').'>Erased</option></select><br>'.
-                        $icon['name'];
-    }
-
-    $total = $cameralife->Database->SelectOne('photos','COUNT(*)',"status=$target_status");
-    echo '</table>';
+  echo '<ul class="thumbnails">';
+  foreach($photos as $photo) {
+    $icon = $photo->GetIcon();
+    echo '<li class="span2"><div class="thumbnail" style="text-align:center">';
+    echo '<a href="'.$icon['href'].'">';
+    echo '<img src="'.$icon['image'].'"></a><br />'.$icon['name'];
+    echo '<select style="width:100%" name="'.$photo->Get('id').'">'.
+                      '<option value="0" '.($target_status==0?'selected':'').'>Public</option>'.
+                      '<option value="1" '.($target_status==1?'selected':'').'>Flagged</option>'.
+                      '<option value="2" '.($target_status==2?'selected':'').'>Private</option>'.
+                      '<option value="3" '.($target_status==3?'selected':'').'>New Upload</option>'.
+                      '<option value="4" '.($target_status==4?'selected':'').'>Erased</option></select><br>';
+    echo '</div></li>';
+  }
+  //$total = $cameralife->Database->SelectOne('photos','COUNT(*)',"status=$target_status");
+  echo '</ul>';
 ?>
 <p>
-  <input type=submit value="Commit Changes">
-  <a href="<?= $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'] ?>">(Undo changes)</a>
+  <input type=submit value="Commit Changes" class="btn btn-danger">
+  <a href="<?= '?'.$_SERVER['QUERY_STRING'] ?>" class="btn">Undo changes</a>
 </p>
 <?php
-  }
-  else // Update DB
-  {
-    echo "<p>Updating the database to reflect any changes to tue PhotoStore...</p>\n<ol>\n";
-    flush();
+}
+else // Update DB
+{
+  echo "<p>Updating the database to reflect any changes to the PhotoStore...</p>\n<ol>\n";
+  flush();
 
-    $output = Folder::Update();
-    foreach($output as $line)
-      echo "<li>$line</li>\n";
+  $output = Folder::Update();
+  foreach($output as $line)
+    echo "<li>$line</li>\n";
 
-    echo "</ol>\n<p>Updating complete :-) Now you can:<ul>\n";
-    echo "<li><a href=\"../search.php?q=unnamed\">Name your new files</a></li>\n";
-    echo "<li><a href='thumbnails.php'>Optimize thumbnails</a></li>\n";
-    echo "</ul>\n";
-  }
+  echo "</ol>\n<p>Updating complete :-) Now you can:<ul>\n";
+  echo "<li><a href=\"../search.php?q=unnamed\">Name your new files</a></li>\n";
+  echo "<li><a href='thumbnails.php'>Optimize thumbnails</a></li>\n";
+  echo "</ul>\n";
+}
 ?>
-  </table>
-  </form>
-</body>
+      </form>
+    </div>
+  </body>
 </html>
-
