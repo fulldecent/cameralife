6ae7fec61abc1eb10f6ff8d0df3aa42cbd2eb5c9
William Entriken <github@phor.net>
2013-03-31 21:00:57 -0400

    add phone home on admin page, light usage tracking

--- admin/index.php
+++ admin/index.php
@@ -42,6 +42,17 @@ $numcomments = $cameralife->Database->SelectOne('comments','COUNT(*)','id>'.($ca
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+    <script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-52764-13']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+    </script>
   </head>
 
   <body>
