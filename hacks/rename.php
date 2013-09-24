080999e2075ed923d1521ca3ee2a44170441ccd5
William Entriken <github@phor.net>
2013-09-24 17:30:12 -0400

    fixes #20

--- hacks/rename.php
+++ hacks/rename.php
@@ -1,152 +1,120 @@
 <?php
-/**An adminisitrative tool-Batch Renamer that names unnamed photos in a batch
- *
- *The following code handles form actions
- *<code>$curphoto = FALSE;
- *foreach ($_POST as $key=>$val)
- *{
- *  list($cmd, $id) = split('_', $key);
- * if ($id == FALSE) continue;
- * if (!$curphoto || $id != $curphoto->Get('id'))
- *  {
- *   $curphoto = new Photo($id);
- * }
- *</code>
- *@author Will Entriken <cameralife@phor.net>
- *@copyright Copyright (c) 2001-2009 Will Entriken
- *@access public
+/*
+ * Administer files on the site
+ * BEWARE: BACKUP YOUR DATABASE BEFORE MESSING AROUND HERE!
+ * @author Will Entriken <cameralife@phor.net>
+ * @copyright Copyright (c) 2001-2013 Will Entriken
+ * @access public
  */
+$features=array('database','security', 'photostore', 'imageprocessing');
+require '../main.inc';
+chdir ($cameralife->base_dir);
+$cameralife->base_url = dirname($cameralife->base_url);
+$cameralife->Security->authorize('admin_customize', 1); // Require
+$_GET['page'] = isset($_GET['page']) ? $_GET['page'] : 'flagged';
+@ini_set('max_execution_time',9000); // for rescan
 
-  $features=array('database','theme','security','imageprocessing','photostore');
-  require '../main.inc';
-  $cameralife->base_url = dirname($cameralife->base_url);
-
-  $cameralife->Security->authorize('admin_file', 1); // Require
-
-  $start = $_GET['start'] or $start = 0;
-  $perpage = $_POST['perpage'] or $perpage = 12;
-
-  // Handle form actions
-    $curphoto = FALSE;
-  foreach ($_POST as $key=>$val) {
-    list($cmd, $id) = explode('_', $key);
-    if ($id == FALSE) continue;
-    if (!$curphoto || $id != $curphoto->Get('id')) {
-      $curphoto = new Photo($id);
-    }
-    switch ($cmd) {
-    case 'desc':
-      if ($curphoto->Get('description') != $val && strlen($val) > 0) {
-        if ($val == 'ERASED')
-          $curphoto->Erase();
-        else
-          $curphoto->Set('description', $val);
-      }
-      break;
-    case 'key':
-      if ($curphoto->Get('keywords') != $val);
-        $curphoto->Set('keywords', $val);
-      break;
-    case 'rot':
-      if ($val != 0)
-        $curphoto->Rotate($val);
-      break;
-    case 'stat':
-      if ($curphoto->Get('status') != $val);
-        $curphoto->Set('status', $val);
-      break;
-    }
+$curphoto = FALSE;
+foreach ($_POST as $key=>$val) {
+  list($cmd, $id) = explode('_', $key);
+  if ($id == FALSE) continue;
+  if (!$curphoto || $id != $curphoto->Get('id')) {
+    $curphoto = new Photo($id);
   }
-
+  switch ($cmd) {
+  case 'desc':
+    if ($curphoto->Get('description') != $val && strlen($val) > 0) {
+      if ($val == 'ERASED')
+        $curphoto->Erase();
+      else
+        $curphoto->Set('description', $val);
+    }
+    break;
+  case 'key':
+    if ($curphoto->Get('keywords') != $val);
+      $curphoto->Set('keywords', $val);
+    break;
+  case 'rot':
+    if ($val != 0)
+      $curphoto->Rotate($val);
+    break;
+  case 'stat':
+    if ($curphoto->Get('status') != $val);
+      $curphoto->Set('status', $val);
+    break;
+  }
+}
 ?>
-<html>
-<head>
-  <title><?= $cameralife->GetPref('sitename') ?> - Batch Renamer</title>
-  <link rel="stylesheet" href="hacks.css">
-  <meta http-equiv="Content-Type" content="text/html; charset= ISO-8859-1">
-  <script language="javascript">
-    function changealldesc()
-    {
-      val = document.getElementById('desc').value;
-      inputs = document.getElementsByTagName('input');
-      for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].name.indexOf('desc_') >= 0) {
-          inputs[i].value=val;
-        }
-      }
-    }
-    function addallkey()
-    {
-      vals = document.getElementById('key').value.split(' ');
-      for (var j = 0; j < vals.length; j++) {
-        val = vals[j];
-        inputs = document.getElementsByTagName('input');
-        for (var i = 0; i < inputs.length; i++) {
-          if (inputs[i].name.indexOf('key_') >= 0) {
-            if ((' '+inputs[i].value+' ').indexOf(' '+val+' ') == -1) {
-              if (inputs[i].value.substr(-1) != ' ' && inputs[i].value.length > 0)
-                inputs[i].value += ' ';
-              inputs[i].value += val;
-            }
-          }
-        }
-      }
-    }
-    function delallkey()
-    {
-      vals = document.getElementById('key').value.split(' ');
-      for (var j = 0; j < vals.length; j++) {
-        val = vals[j];
-        inputs = document.getElementsByTagName('input');
-        for (var i = 0; i < inputs.length; i++) {
-          if (inputs[i].name.indexOf('key_') >= 0) {
-            re = new RegExp(' '+val+' ');
-            if (re.test(' '+inputs[i].value+' ')) {
-              newval = (' '+inputs[i].value+' ').replace(re,' ');
-              inputs[i].value = newval.substr(1, newval.length-2);
-            }
-          }
-        }
-      }
-    }
-  </script>
-</head>
-<body>
-<form id="form1" method="post"> <!--action="<?= $_SERVER['PHP_SELF'] ?>&#63;start=<?= $_GET['start']?>"-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Camera Life - Hacks</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-<div id="header">
-<h1>Hacks - Rename</h1>
+    <!-- Le styles -->
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <span class="navbar-brand"><a href="../"><?= $cameralife->GetPref("sitename") ?></a> / Hacks</span>
+      </div>
+    </div>
+    <div class="container">
+      <form method="post">
+        <p class="lead">Photos without a name set are shown below, you can quickly edit unnamed photos.</p>
 <?php
-  $home = $cameralife->GetIcon('small');
-  echo '<a href="'.$home['href']."\"><img src=\"".$cameralife->IconURL('small-main')."\">".$home['name']."</a>\n";
-?> |
-<a href="../stats.php"><img src="<?= $cameralife->IconURL('small-photo')?>">Stats</a> |
-<a href="../admin/index.php"><img src="<?= $cameralife->IconURL('small-admin')?>">Administration</a>
-</div>
 
-<?php
-  echo "<p>Show <select name=\"perpage\" onchange=\"document.getElementById('form1').submit()\">";
-  foreach (array(8,12,25,100,'all') as $num) {
-    if ($perpage == $num)
-      echo "<option selected>$num</option>\n";
-    else
-      echo "<option>$num</option>\n";
-  }
-  echo "</select> photos per page. <input type=submit value=\"Change\"></p>";
+  $search = new Search('unnamed');
+#  $search->mySearchPhotoCondition = "status=$target_status OR 0";
+  $search->SetPage(0, 24);
+  $photos = $search->GetPhotos();
+  $icons = array();
 
-  echo "<table><tr><td>All descriptions:<td><input id='desc' name='desc' size=35><td><input type=button value=\"Change all\" onclick=\"changealldesc()\">";
-  echo "<tr><td>All keywords:<td><input id='key' name='key' size=35><td><input type=button value=\"Add to all\" onclick=\"addallkey()\"><input type=button value=\"Remove from all\" onclick=\"delallkey()\"></table>";
-  echo "<hr size=1>";
-
-  $selection = "id, description, keywords, status, mtime";
-  if ($perpage == 'all') $limit = '';
-  else $limit = "LIMIT $start, $perpage";
-  $query = $cameralife->Database->Select('photos',$selection,"description='unnamed' AND (status=0 OR status=2)","ORDER BY id $limit");
-
-  echo "<table>";
-  while ($photo = $query->FetchAssoc()) {
-    if ($i++%2==0) echo "<tr>";
-    echo "<td>";
+  echo '<div class="thumbnails">';
+  $i=0;
+  foreach ($photos as $photo) {
+    $icon = $photo->GetIcon();
+    $target_status = $photo->Get('status');
+    echo '<div class="col-sm-2"><div class="thumbnail text-center">';
+    echo '<a href="'.$icon['href'].'">';
+    echo '<img id="img_'.$photo->Get('id').'" src="'.$icon['image'].'"></a>';
+/*
+    echo '<select class="form-control" style="width:100%" name="'.$photo->Get('id').'">'.
+                      '<option value="0" '.($target_status==0?'selected':'').'>Public</option>'.
+                      '<option value="1" '.($target_status==1?'selected':'').'>Flagged</option>'.
+                      '<option value="2" '.($target_status==2?'selected':'').'>Private</option>'.
+                      '<option value="3" '.($target_status==3?'selected':'').'>New Upload</option>'.
+                      '<option value="4" '.($target_status==4?'selected':'').'>Erased</option></select><br>';
+*/
+    echo '<input class="form-control" id="desc_'.$photo->Get('id').'" name="desc_'.$photo->Get('id').'" value="'.$photo->Get('description').'">';
+/*
+    echo '<div class="btn-group btn-group-justified" data-toggle="buttons-radio">
+    <a type="button" name="toggle'.$photo->Get('id').'" data-value="" class="btn btn-default myButton"><i class="icon-shield"></i></a>
+    <a type="button" name="toggle'.$photo->Get('id').'" class="btn btn-default myButton"><i class="icon-shield icon-rotate-90"></i></a>
+    <a type="button" name="toggle'.$photo->Get('id').'" class="btn btn-default myButton"><i class="icon-shield icon-rotate-180"></i></a>
+    <a type="button" name="toggle'.$photo->Get('id').'" class="btn btn-default myButton"><i class="icon-shield icon-rotate-270"></i></a>
+</div>';
+*/
+    echo "<input checked type=radio id=\"rot0_".$photo->Get('id')."\" name=\"rot_".$photo->Get('id')."\" value=\"0\" onclick=\"document.getElementById('img_".$photo->Get('id')."').src='rotatethumb.php?id=".$photo->Get('id')."&amp;rotate=0'\">";
+    echo "<label for=\"rot1_".$photo->Get('id')."\"><i class='icon-shield'></i></label> ";
+    echo "<input type=radio id=\"rot90_".$photo->Get('id')."\" name=\"rot_".$photo->Get('id')."\" value=\"90\" onclick=\"document.getElementById('img_".$photo->Get('id')."').src='rotatethumb.php?id=".$photo->Get('id')."&amp;rotate=90'\">";
+    echo "<label for=\"rot90_".$photo->Get('id')."\"><i class='icon-shield icon-rotate-90'></i></label> ";
+    echo "<input type=radio id=\"rot180_".$photo->Get('id')."\" name=\"rot_".$photo->Get('id')."\" value=\"180\" onclick=\"document.getElementById('img_".$photo->Get('id')."').src='rotatethumb.php?id=".$photo->Get('id')."&amp;rotate=180'\">";
+    echo "<label for=\"rot180_".$photo->Get('id')."\"><i class='icon-shield icon-rotate-180'></i></label> ";
+    echo "<input type=radio id=\"rot270_".$photo->Get('id')."\" name=\"rot_".$photo->Get('id')."\" value=\"270\" onclick=\"document.getElementById('img_".$photo->Get('id')."').src='rotatethumb.php?id=".$photo->Get('id')."&amp;rotate=270'\">";
+    echo "<label for=\"rot270_".$photo->Get('id')."\"><i class='icon-shield icon-rotate-270'></i></label><br>";
+/*
     echo "<a href=\"../photo.php&#63;id=".$photo['id']."\">";
     echo "<img id=\"img_".$photo['id']."\" src='../media.php&#63;scale=thumbnail&amp;id=".$photo['id'].'&amp;ver='.$photo['mtime']."'></a>";
     echo "<td width=\"50%\">";
@@ -168,27 +136,19 @@
     echo "<label for=\"stat2_".$photo['id']."\">Private</label><br>";
     echo "<input type=button value=\"Erase\" onclick=\"document.getElementById('desc_".$photo['id']."').value='ERASED'\">\n";
     echo "<input type=button value=\"Reset\" onclick=\"document.getElementById('desc_".$photo['id']."').value='".$photo['description']."'\">\n";
+*/
 
+    echo '</div></div>';
+    if (++$i%6==0) echo '</div><div class="thumbnails" style="clear:left">';
   }
-  echo "</table>";
-
-  #$cameralife->Theme->Grid($query);
-  $total = $cameralife->Database->SelectOne('photos','COUNT(*)',"description='unnamed'");
+  //$total = $cameralife->Database->SelectOne('photos','COUNT(*)',"status=$target_status");
+  echo '</div>';
 ?>
-<p>
-  <input type=submit value="Commit Changes">
-  <a href="&#63;start=<?= $start ?>">(Undo changes)</a>
+<p style="clear:left">
+  <input type=submit value="Commit Changes" class="btn btn-danger">
+  <a href="<?= '?'.$_SERVER['QUERY_STRING'] ?>" class="btn">Undo changes</a>
 </p>
-<?php
-
-  for($i=max(0,(floor($_GET['start']/$perpage)-2)*$perpage); $i<min($total,$_GET['start']+3*$perpage); $i+=$perpage)
-    if ($i==$_GET['start'])
-      echo "Page ".($i/$perpage)." ";
-    else
-      echo "<a href=\"&#63;start=$i\">Page ".($i/$perpage)."</a>  ";
-  echo "$total total photos."
-?>
-  </table>
-  </form>
-</body>
+      </form>
+    </div>
+  </body>
 </html>
